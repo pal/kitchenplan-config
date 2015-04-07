4010497b1b066366d47ec9ca1a7b4966d14451fe
Pål Brattberg <brattberg@gmail.com>
2015-04-07 23:24:16 +0200

    Naive implementation of single call shell file

--- go.sh
+++ go.sh
@@ -1,6 +1,22 @@
-# Source: http://codecrate.com/2013/11/cooking-osx-machines-with-kitchenplan.html
-# configure location of custom kitchenplan repository
+# Automated setup of machine config, thanks to pliljenberg!
 export KITCHENPLAN_REPO=https://github.com/pal/kitchenplan-config.git
 
-# install/update kitchenplan recipes and run chef
-ruby -e "$(curl -fsSL https://raw.github.com/kitchenplan/kitchenplan/master/go)"
+# Increase the sudo timeout (since kitchenplan provision will take some time):
+sh set_longer_sudoers_timeout.sh
+
+# Install Kitchenplan and run it (takes a loooong time)
+sudo gem install kitchenplan
+kitchenplan setup
+kitchenplan provision
+
+# Install Homeshick
+git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+
+# Get my homeshick profile files
+homeshick clone pal/profile
+ln -s $HOME/.profile.d/init $HOME/.profile
+
+# Switch to Bash4 shell
+echo "/usr/local/bin/bash" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/bash
